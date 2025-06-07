1ea33984c7142035ce5d672e81c5c4cdaed2db7a
Ruslan Khabibullin <yronwooz@gmail.com>
2025-06-07 17:17:27 +0300

    try-catch improved, scanner closing

--- 01/src/org/yroqwooz/evenchecker/Main.java
+++ 01/src/org/yroqwooz/evenchecker/Main.java
@@ -6,16 +6,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int num = 0;
-        try {
-            Scanner sc = new Scanner(System.in);
-            num = sc.nextInt();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        Scanner sc = new Scanner(System.in);
+        boolean valid = false;
+        while (!valid) {
+            try {
+                num = sc.nextInt();
+                valid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Введите число");
+            }
         }
-            System.out.println("Number: " + num);
-            System.out.println("Division by  2: " + NumberAnalyzer.isEven(num));
-            System.out.println("Division by  3: " + NumberAnalyzer.isDivideBy3(num));
-            System.out.println("Division by  5: " + NumberAnalyzer.isDivideBy5(num));
-            System.out.println("Division by 10: " + NumberAnalyzer.isDivideBy10(num));
+        System.out.println("Number: " + num);
+        System.out.println("Division by  2: " + NumberAnalyzer.isEven(num));
+        System.out.println("Division by  3: " + NumberAnalyzer.isDivideBy3(num));
+        System.out.println("Division by  5: " + NumberAnalyzer.isDivideBy5(num));
+        System.out.println("Division by 10: " + NumberAnalyzer.isDivideBy10(num));
+        sc.close();
     }
 }
