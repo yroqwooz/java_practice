5e3858d32e36dd1cee2e92bbc4decd6d9419f29e
Ruslan Khabibullin <yronwooz@gmail.com>
2025-06-16 18:26:58 +0300

    Warning clean and filling menu with options

--- 04/src/org/yroqwooz/MovieCatalogue/MovieMenu.java
+++ 04/src/org/yroqwooz/MovieCatalogue/MovieMenu.java
@@ -30,14 +30,19 @@ public class MovieMenu {
 
     private static void printMenu() {
         System.out.println("""
-                    1 - Показать все фильмы
-                    2 - Добавить фильм
-                    3 - Фильмы по режиссёру
-                    4 - Классические фильмы
-                    5 - Фильмы с высоким рейтингом
-                    6 - Ипортировать из JSON
-                    7 - Экспортировать в JSON
-                    8 - Отсортировать по годам
+                     1 - Показать все фильмы
+                     2 - Добавить фильм
+                     3 - Фильмы по режиссёру...
+                     4 - Классические фильмы
+                     5 - Фильмы с высоким рейтингом
+                     6 - Ипортировать из JSON
+                     7 - Экспортировать в JSON
+                     8 - Отсортировать по годам
+                     9 - Фильмы до года...
+                    10 = Фильмы в промежутке лет...
+                    11 - Фильмы конкретного года...
+                    12 - Фильмы по названию...
+                    
                     q - Выход""");
     }
 
@@ -51,7 +56,10 @@ public class MovieMenu {
         actions.put("6", MovieMenu::handleImportFromJson);
         actions.put("7", MovieMenu::handleExportToJson);
         actions.put("8", MovieMenu::handleSortedByYear);
-        actions.put("9", MovieMenu::handleMoviesBetweenYears);
+        actions.put("9", MovieMenu::handleMoviesAfterYear);
+        actions.put("10", MovieMenu::handleMoviesBetweenYears);
+        actions.put("11", MovieMenu::handleMoviesByYear);
+        actions.put("12", MovieMenu::handleMovieByTitle);
     }
 
     private static void handleSeedMovies() {
@@ -99,10 +107,22 @@ public class MovieMenu {
         printList(lib.getMoviesSortedByYear());
     }
 
+    private static void handleMoviesAfterYear() {
+        printList(lib.getMoviesAfterYear(UserInput.inputYear(sc)));
+    }
+
     private static void handleMoviesBetweenYears() {
         int[] year = UserInput.inputYearRange(sc);
         List<Movie> list = lib.getMoviesByYearRange(year);
         printList(list);
+    }
+
+    private static void handleMoviesByYear() {
+        printList(lib.getMoviesByYear(UserInput.inputYear(sc)));
+    }
+
+    private static void handleMovieByTitle() {
+        printList(lib.getMoviesByTitle(UserInput.inputString(sc, "Введите название фильма: ")));
     }
 
     private static void handleInvalidChoice() {
